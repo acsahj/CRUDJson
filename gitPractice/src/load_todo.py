ffa15905a0e67b92f07623d941c9856f53aeb653
acsahj <acsahmathew17@gmail.com>
2024-11-28 08:53:39 +0530

    Added data directory and get_to_details()

--- gitPractice/src/load_todo.py
+++ gitPractice/src/load_todo.py
@@ -11,26 +11,43 @@ If there’s an error reading or parsing the JSON file, log the error and handle
 import json
 
 class jsonWork:
-    def load_list(self):
+    file_path= "/Users/apple/Documents/CRUDJson/gitPractice/Data/todos.json"
+    @classmethod
+    def load_list(cls):
         # base_path = "/Users/acsah/technicalTester"
         # file_path = "base_path/gitPractice/src/todos.json"
         try: 
-            with open(file_path,"r") as file:
+            with open(cls.file_path,"r") as file:
                 data=json.load(file)
-                print("To dos from JSON file:",data)
+                #print("To dos from JSON file:",data)
             return data
         except FileNotFoundError:
-         print(f"Error: The file at {file_path}was not found")
+         print(f"Error: The file at {cls.file_path}was not found")
 
         except json.JSONDecodeError:
-            print(f"Error:The file at {file_path} contains invalid JSON")
+            print(f"Error:The file at {cls.file_path} contains invalid JSON")
     
         except Exception as e:
             print("An unexpected error occurred : {e}")
         return []
-toDo = jsonWork()
-toDo.load_list()
+    
+    @classmethod
+    def get_todo_details(cls,todo_id):
+        try:
+            toDos=cls.load_list()
+            toDo_list= toDos.get("todo",[])
+            for todo in toDo_list:
+                if todo.get("id")==todo_id:
+                    print(f"To do details for{todo_id}:{todo} ")
+                    return todo
+        except ValueError as ve:
+            print(f"ValueError: {ve}")
+        except Exception as e:
+            print(f"An unexpected error occurred in get_todo: {e}")
+        return None
 
 
     
 
+# jsonWork.load_list()
+# jsonWork.get_todo_details(2)
